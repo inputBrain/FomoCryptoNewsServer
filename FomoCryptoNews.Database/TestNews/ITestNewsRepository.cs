0db44f90bc894ddbbed5f79b5f8aa0458fe5a024
Alexander <vsany@meta.ua>
2024-07-08 00:56:58 +0300

    [-] removed test news

--- FomoCryptoNews.Database/TestNews/ITestNewsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace FomoCryptoNews.Database.TestNews;
-
-public interface ITestNewsRepository
-{
-    public Task<TestNewsModel> CreateModel(string title, string description, string? cover);
-    public Task<TestNewsModel> GetOne(int id);
-}
