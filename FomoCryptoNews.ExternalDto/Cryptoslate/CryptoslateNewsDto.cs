01cc1d4c9ba6854ed074b032c5de8cb628e3b836
Alexander <vsany@meta.ua>
2024-07-07 16:58:58 +0300

    news dto

---
+++ FomoCryptoNews.ExternalDto/Cryptoslate/CryptoslateNewsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FomoCryptoNews.ExternalDto.Cryptoslate;
+
+public class CryptoslateNewsDto
+{
+    public string? Title { get; set; }
+    
+    public string? Cover { get; set; }
+
+    public string? Description { get; set; }
+}
