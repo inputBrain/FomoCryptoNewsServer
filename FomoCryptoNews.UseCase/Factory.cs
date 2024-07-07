f7afaac916b7a0ccb57ef76ff3de4c7421ac34df
Alexander <vsany@meta.ua>
2024-07-07 19:17:48 +0300

    [*] small method name fix for Factory in UseCase

--- FomoCryptoNews.UseCase/Factory.cs
+++ FomoCryptoNews.UseCase/Factory.cs
@@ -5,7 +5,7 @@ namespace FomoCryptoNews.UseCase;
 
 public static class Factory
 {
-    public static UseCaseContainer Cteate(ILoggerFactory loggerFactory, ICryptoslateParser cryptoslateParser)
+    public static UseCaseContainer Create(ILoggerFactory loggerFactory, ICryptoslateParser cryptoslateParser)
     {
         return new UseCaseContainer(
             new CryptoslateUseCase(loggerFactory.CreateLogger<CryptoslateUseCase>(), cryptoslateParser)
