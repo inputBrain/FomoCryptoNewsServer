3acabfb1d3dfd82aabf85423cbea0aee53d499eb
Alexander <vsany@meta.ua>
2024-07-08 02:48:58 +0300

    [+] Endpoints: GetAll (news) GetAllByStatus, ApproveNews

--- FomoCryptoNews.Host/Controllers/Client/NewsController.cs
+++ FomoCryptoNews.Host/Controllers/Client/NewsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using FomoCryptoNews.Database;
+using FomoCryptoNews.Database.Cryptoslate;
 using FomoCryptoNews.Host.Configs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,32 +13,59 @@ namespace FomoCryptoNews.Host.Controllers.Client;
 public class NewsController : AbstractController<NewsController>
 {
     private readonly ITelegramBotClient _botClient;
+
     private readonly TelegramBotConfig _telegramBotConfig;
 
-    
-    public NewsController(PostgreSqlContext context, ILoggerFactory loggerFactory, ITelegramBotClient botClient, IOptions<TelegramBotConfig> telegramBotConfig) : base(context, loggerFactory)
+
+    public NewsController(
+        PostgreSqlContext context,
+        ILoggerFactory loggerFactory,
+        ITelegramBotClient botClient,
+        IOptions<TelegramBotConfig> telegramBotConfig
+    ) : base(context, loggerFactory)
     {
         _botClient = botClient;
         _telegramBotConfig = telegramBotConfig.Value;
     }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateModel(string title, string description, string cover)
-    {
-        var model = await Db.NewsRepository.CreateModel(title, description, cover);
 
-        return SendOk(model);
+    // [HttpPost]
+    // public async Task<IActionResult> CreateModel(string? title, string description, string cover)
+    // {
+    //     var model = await Db.CryptoslateNewsRepository.CreateModel(title, description, cover);
+    //
+    //     return SendOk(model);
+    // }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllByStatus(Status status)
+    {
+        var collection = await Db.CryptoslateNewsRepository.ListAllByStatus(status);
+
+        return SendOk(collection);
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var collection = await Db.CryptoslateNewsRepository.ListAll();
+
+        return SendOk(collection);
     }
 
 
     [HttpGet]
     public async Task<IActionResult> ApproveNews(int newsId)
     {
-        var model = await Db.NewsRepository.GetOne(newsId);
+        var model = await Db.CryptoslateNewsRepository.GetOne(newsId);
 
         var preparedMessage = $"<b>{model.Title}</b>\n\n{model.Description}";
-        
+
         await _botClient.SendTextMessageAsync(_telegramBotConfig.PublicChannelId, preparedMessage, disableWebPagePreview: true, parseMode: ParseMode.Html);
+
+        await Db.CryptoslateNewsRepository.UpdateStatus(model, Status.Approved);
 
         return Ok();
     }
