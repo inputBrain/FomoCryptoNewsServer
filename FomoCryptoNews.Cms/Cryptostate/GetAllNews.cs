7c8c26c0a4f51f06a0653bf6fa102cc5e42c632c
Alexander <vsany@meta.ua>
2024-08-14 05:10:32 +0300

    [+] Request and Response for List All News endpoint

---
+++ FomoCryptoNews.Cms/Cryptostate/GetAllNews.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FomoCryptoNews.Cms.Payload.Cryptostate;
+
+namespace FomoCryptoNews.Cms.Cryptostate;
+
+public sealed class GetAllNews
+{
+        
+    [Required]
+    public int Skip { get; set; }
+    
+    [Required]
+    public int Take { get; set; }
+
+
+    public class Response : AbstractResponse
+    {
+        [Required]
+        public List<CryptostateNews> News { get; }
+        
+        public int TotalCount { get; }
+
+
+        public Response(List<CryptostateNews> news, int totalCount)
+        {
+            News = news;
+            TotalCount = totalCount;
+        }
+    } 
+}
