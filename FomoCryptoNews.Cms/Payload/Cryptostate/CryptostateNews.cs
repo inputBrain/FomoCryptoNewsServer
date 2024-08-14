99a0c8d62783995fbe01757273c57f82e3fab2e4
Alexander <vsany@meta.ua>
2024-08-14 05:09:48 +0300

    [+] added news Payload model

---
+++ FomoCryptoNews.Cms/Payload/Cryptostate/CryptostateNews.cs
@@ -0,0 +1,18 @@
+﻿namespace FomoCryptoNews.Cms.Payload.Cryptostate;
+
+public class CryptostateNews
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Cover { get; set; }
+
+    public StatusPayload StatusPayload { get; set; }
+
+    public int CreatedAt { get; set; }
+
+    public int UpdatedAt { get; set; }
+}
